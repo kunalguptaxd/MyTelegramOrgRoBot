97ca71b4b6a81f8d5364a21b091678d764285fb4
kunalguptaxd <64132823+kunalguptaxd@users.noreply.github.com>
2020-06-28 14:40:51 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
+If You want Any Help So DM @MrJetex
 Thank you for using me 😬
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
@@ -14,7 +14,7 @@ by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pi
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@MrJetex"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
