430065561bbde7193f549a69a36755bed9a8e95a
kunalguptaxd <64132823+kunalguptaxd@users.noreply.github.com>
2020-06-28 14:47:58 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi!
-If You want Any Help So DM @MrJetex
+please read the TnC before proceeding: https://t.me/SpEcHlDe/889
 Thank you for using me 😬
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
